70ccb994025b57c9b2ee4ffbe8eb4c9f3c858bc9
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-12-04 15:21:19 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+
+// this will ne used to create the bot
+require("dotenv/env");
+const {Client} = require('discord.js');
+const {OpenAi} = require('openai');
+
+
+const client = new Client({
+    intents: ['Guilds', 'GuildMembers', 'GuildMessages', "MessagesContent"]
+})
+
+
+// this is used to make the  following event holder 
+client.on("the bot is ready", () =>{
+    console.log("bot online")
+})
+
+client.login(process.env.Token)
+
+
+console.log(response)
+function Discord_bot(){
+    console.log("Hello World")
+}
+
+discord = Discord_bot()
+
