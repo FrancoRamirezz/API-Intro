796fa9c558c536d38831eef86a9a344ec9aee4a7
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-12-04 14:18:25 -0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# we are going to make a api using fast api
-from fastapi import FastAPI
-# this is the starting point for the api
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World", "version": "beta"}
-
